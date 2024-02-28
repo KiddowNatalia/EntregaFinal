c9c7632c502d67603ad33136ed58bab112cd7eee
KiddowNatalia <143464283+KiddowNatalia@users.noreply.github.com>
2024-02-28 20:13:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,113 +1,196 @@
+import fetchDataPeliculas from "./customApi.js";
 
-function Pelicula(nombre, cateogria) {
-  this.nombre = nombre;
-  this.cateogria = cateogria;
-}
+async function modificarDOM(){
+  try{
+    const dataPeliculas = await fetchDataPeliculas();
+      const contenedorPrincipal = document.getElementById("contenedor-principal");
+      const peliculaElegida = [];
 
-const peliculaUno = new Pelicula("Openhaimer", 'Drama');
-const peliculaDos = new Pelicula("Barbie", 'Comedia');
-const peliculaTres = new Pelicula("Paraistes", 'Drama');
-const peliculaCuatro = new Pelicula("El Conjuro", 'Terror');
+      const añadirMensajePeliculaSeleccionada = (peliculaElegida) => {
+        const contenedorTextoCompra = document.getElementsByClassName(
+          "contenedorTextoCompra"
+        )[0];
+        const textoContenedorCompra = document.createElement("p");
+        textoContenedorCompra.classList.add("peliculaSeleccionada");
+        textoContenedorCompra.style.display = "block";
 
-const peliculas = [peliculaUno, peliculaDos, peliculaTres, peliculaCuatro];
-const funciones = ['Lunes 22hs', 'Martes 20:30hs', 'Viernes 22:30hs', 'Sabado 21hs'];
-const precioBoleto = 2000;
-const compraFinal = [];
-let confirmacionDeCompra = 1;
+        textoContenedorCompra.textContent = `Esta por comprar entradas para la pelicula:  ${peliculaElegida.titulo}.`;
+        contenedorTextoCompra.appendChild(textoContenedorCompra);
+      };
 
-const elegirPelicula = () => {
-  let pelicula;
-  do {
-    pelicula = Number(
-      prompt(
-        `Escoja que película quiere ver. Seleccione el número de la opción deseada. \n\n 1. ${peliculaUno.nombre}  \n 2. ${peliculaDos.nombre}  \n 3. ${peliculaTres.nombre}  \n 4. ${peliculaCuatro.nombre} `
-      )
-    );
-  } while (pelicula < 0 || pelicula > 4);
-  return pelicula;
-}
+      const añadirPrecioEntradas = (valorTotal) => {
+        const contenedorConfirmarCompra =
+          document.getElementsByClassName("confirmarCompra")[0];
+        const textoValorEntradas = document.createElement("p");
+        textoValorEntradas.classList.add("seleccionarEntradas");
+        textoValorEntradas.style.display = "block";
+        textoValorEntradas.textContent = `El valor total es de: ${valorTotal}`;
+        contenedorConfirmarCompra.appendChild(textoValorEntradas);
+      };
 
-const elegirFuncion = () => {
-  let funcion;
-  do {
-    funcion = Number(
-      prompt(
-        `Escoja el día y horario para ${peliculas[compraFinal[0]]}. \n\n 1. ${funciones[0]}  \n 2. ${funciones[1]}  \n 3. ${funciones[2]}  \n 4. ${funciones[3]} `
-      )
-    );
-      
-  } while (funcion < 0 || funcion > 4);
-  return funcion;
-}
+      const realizarCompra = (peliculaElegida) => {
+        const contenedorCompraEntradas = document.getElementById(
+          "contenedor-compra-entradas"
+        );
+        contenedorCompraEntradas.style.display = "flex";
 
-const elegirEntradas = () => {
-  let entradas;
-  entradas = Number(
-    prompt(
-      `Escoja el numero de entradas. El precio por cada una es:  ${precioBoleto}. `
-    )
-  );
-  return entradas;
-}
+        const contenedorPeliculaSeleccionada = document.getElementsByClassName(
+          "peliculaSeleccionada"
+        )[0];
 
-const confirmarCompra = () => {
-  let confirmaciónDeCompra;
-  console.log(compraFinal);
-  do {
-    confirmaciónDeCompra = Number(
-      prompt(
-        `Su compra es para la película: ${compraFinal[0]} en la función ${compraFinal[1]}. El total por ${compraFinal[2]} entradas es: ${compraFinal[2] * precioBoleto}. \n\n ¿Desea confirmar su compra? \n\n 1. Confirmar \n\n 2. Modificar compra \n\n 3. Cancelar`
-      )
-    );
-  } while (confirmaciónDeCompra < 0 || confirmaciónDeCompra > 3);
-  return confirmaciónDeCompra;
-}
+        const botonCalcular = document.getElementsByClassName("botonCalcular")[0];
 
-const reemplazarCompra = (valorAnterior, valorNuevo) => {
-  compraFinal.forEach((elemento, index, array) => {
-    if(elemento === valorAnterior){
-       array[index] = valorNuevo;
-    }
-  });
-}
+        botonCalcular.addEventListener("click", () => {
+          const inputEntradas = document.getElementById("entradas").value;
+          const numeroDeEntradas = parseInt(inputEntradas);
+          if (numeroDeEntradas > 0) {
+            const valorTotal = numeroDeEntradas * 2000;
+            localStorage.setItem("entradas", JSON.stringify(valorTotal));
+            localStorage.setItem("pelicula", JSON.stringify(peliculaElegida));
+            const contenedorPrecioEntradas = document.getElementsByClassName(
+              "seleccionarEntradas"
+            )[0];
+            if (contenedorPrecioEntradas) {
+              contenedorPrecioEntradas.remove();
+              añadirPrecioEntradas(valorTotal);
+            }
+          }
+        });
 
-const modificarCompra = (peliculaPrevia, funcionPrevia, entradaPrevia) => {
-  let modificacion = Number(prompt("¿Que parte de la compra desea modificar? \n\n 1. Película \n\n 2. Función \n\n 3. Cantidad de entradas"));
-  switch(modificacion){
-    case 1:
-      const nuevaPelicula = elegirPelicula();
-      reemplazarCompra(peliculaPrevia, peliculas[nuevaPelicula - 1].nombre);
-      break;
-    case 2:
-      const nuevaFuncion = elegirFuncion();
-      reemplazarCompra(funcionPrevia, funciones[nuevaFuncion - 1]);
-      break;
-    case 3:
-      const nuevaEntrada = elegirEntradas();
-      reemplazarCompra(entradaPrevia, nuevaEntrada);
-      break;
-    default:
-        break;
+        if (contenedorPeliculaSeleccionada) {
+          contenedorPeliculaSeleccionada.remove();
+          añadirMensajePeliculaSeleccionada(peliculaElegida);
+        }
+
+        const botonFinalizar = document.getElementsByClassName("botonFinalizar")[0];
+
+        botonFinalizar.addEventListener("click", () => {
+          const entradasLocalStorage = JSON.parse(localStorage.getItem("entradas"));
+          const peliculaLocalStorage = JSON.parse(localStorage.getItem("pelicula"));
+
+          if (entradasLocalStorage > 0) {
+
+            Swal.fire({
+              title: "¿Quieres confirmar tu compra?",
+              text: `Película: ${peliculaLocalStorage.titulo} \n\n Precio: ${entradasLocalStorage} `,
+              showCancelButton: true,
+              confirmButtonText: "Confirmar",
+              denyButtonText: "Cancelar"
+            }).then((result) => {
+              if (result.isConfirmed) {
+                Swal.fire("¡Compra confirmada, muchas gracias!", "", "success");
+              } else if (result.isDismissed) {
+                Swal.fire("La compra ha sido cancelada", "", "error");
+              }
+            });
+
+            
+          } else {
+            Toastify({
+              text: `Ingrese el numero de entradas antes de confirmar`,
+              duration: 2000,
+              newWindow: true,
+              close: true,
+              gravity: "top", 
+              position: "left", 
+              stopOnFocus: true, 
+              style: {
+                background: "linear-gradient(to right, #FF4633 , #FC6758 )",
+              }
+            }).showToast();
+          }
+        });
+      };
+
+      const insertarPeliculas = () => {
+        const peliculasAgregadas = [];
+        const contenedorPeliculas_1 = document.createElement("div");
+        const contenedorPeliculas_2 = document.createElement("div");
+        contenedorPeliculas_1.classList.add("contenedorPeliculas_1");
+        contenedorPeliculas_2.classList.add("contenedorPeliculas_2");
+        Object.keys(dataPeliculas).forEach(key => {
+          const pelicula = dataPeliculas[key];
+          if( key !== 'id'){      
+          const contenedorPelicula = document.createElement("div");
+          contenedorPelicula.classList.add("pelicula");
+          const tituloPelicula = document.createElement("h2");
+          tituloPelicula.textContent = pelicula.titulo;
+
+          const imagenPelicula = document.createElement("img");
+          imagenPelicula.src = pelicula.imagen;
+          imagenPelicula.alt = pelicula.titulo;
+          imagenPelicula.classList.add("imagen");
+
+          const descripcionPelicula = document.createElement("p");
+          descripcionPelicula.textContent = pelicula.descripcion;
+
+          const botonAgregarPelicula = document.createElement("button");
+          botonAgregarPelicula.classList.add("boton");
+          botonAgregarPelicula.textContent = "Seleccionar";
+
+          botonAgregarPelicula.addEventListener("click", () => {
+            if (peliculaElegida.length === 1) {
+              peliculaElegida.splice(0, 1);
+              peliculaElegida.push(pelicula);
+              realizarCompra(pelicula);
+            } else {
+              peliculaElegida.push(pelicula);
+              realizarCompra(pelicula);
+            }
+
+            Toastify({
+              text: `¡Felicidades! Ha seleccionado la película: ${pelicula.titulo}`,
+              duration: 2000,
+              newWindow: true,
+              close: true,
+              gravity: "top", 
+              position: "left", 
+              stopOnFocus: true, 
+              style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+              }
+            }).showToast();
+
+            setTimeout(()=>{
+              Toastify({
+              text: `Proceda a la sección de confirmar compra`,
+              duration: 3000,
+              newWindow: true,
+              close: true,
+              gravity: "bottom", 
+              position: "left", 
+              stopOnFocus: true, 
+              style: {
+                background: "linear-gradient(to right, #FFB833, #FF8633)",
+              }
+            }).showToast();
+            }, 1200)
+          
+          });
+
+          contenedorPelicula.appendChild(tituloPelicula);
+          contenedorPelicula.appendChild(imagenPelicula);
+          contenedorPelicula.appendChild(descripcionPelicula);
+          contenedorPelicula.appendChild(botonAgregarPelicula);
+
+          peliculasAgregadas.push(pelicula);
+          if (peliculasAgregadas.length <= 3) {
+            contenedorPeliculas_1.appendChild(contenedorPelicula);
+          } else {
+            contenedorPeliculas_2.appendChild(contenedorPelicula);
+          }
+
+          contenedorPrincipal.appendChild(contenedorPeliculas_1);
+          contenedorPrincipal.appendChild(contenedorPeliculas_2);
+          }
+        });
+      };
+
+      insertarPeliculas();
+   
+  } catch(error){
+    console.error('Error en el catch (usar google chrome para ver el proyecto):', error);
   }
-  confirmacionDeCompra = confirmarCompra();
 }
 
-const peliculaElegida = elegirPelicula();
-if(peliculas[peliculaElegida - 1]?.nombre !== undefined){
-compraFinal.push(peliculas[peliculaElegida - 1].nombre);
-}
-
-const funcionElegida = elegirFuncion(peliculaElegida);
-compraFinal.push(funciones[funcionElegida - 1]);
-
-const numeroDeEntradas = elegirEntradas();
-compraFinal.push(numeroDeEntradas);
-
-confirmacionDeCompra = confirmarCompra();
-
-do{
-  modificarCompra(compraFinal[0], compraFinal[1], compraFinal[2]);    
-} while(confirmacionDeCompra === 2);
-
-alert('¡Gracias por su compra!')
-        
+modificarDOM();
